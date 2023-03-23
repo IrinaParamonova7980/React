0f1b535b1c5b515f845a46c35f5cbd098864b3ce
IrinaParamonova7980 <irisha-7980@yandex.ru>
2023-03-23 11:31:06 +0300

    rates theme

--- rates/src/App.js
+++ rates/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Rates from "./components/Rates";
-import colorA from "./components/themeA.scss";
+import colorA from "./components/themeA.module.scss";
+
 //import Login from "./components/Login/Login";
-import Component from "./components/Component/Component";
-import themelight from "./components/Component/themelight.css";
-import themedark from "./components/Component/themedark.css";
+// import Component from "./components/Component/Component";
+// import themelight from "./components/Component/themelight.module.scss";
+// import themedark from "./components/Component/themedark.module.scss";
 
 //const isAutorized = "Маруся";
 
@@ -35,20 +36,21 @@ const rates = [
 function App() {
   return (
     <div className="App">
-      {rates.map((rate) => (
+      {rates.map((rate, index) => (
         <Rates
           name={rate.name}
           price={rate.price}
           speed={rate.speed}
           isSelected={rate.isSelected}
           theme={colorA}
+          key={index}
         ></Rates>
       ))}
-      <div>
+      {/* <div>
         <Component theme={themedark}></Component>
         <Component theme={themelight}></Component>
-        {/* <Login isAutorized={isAutorized} name={isAutorized}></Login> */}
-      </div>
+        <Login isAutorized={isAutorized} name={isAutorized}></Login>
+      </div> */}
     </div>
   );
 }
