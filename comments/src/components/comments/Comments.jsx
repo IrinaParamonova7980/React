87edc199a7687ee641b4215fb111882bc04acaf6
IrinaParamonova7980 <irisha-7980@yandex.ru>
2023-05-12 16:24:08 +0300

    Comments functional

--- comments/src/components/comments/Comments.jsx
+++ comments/src/components/comments/Comments.jsx
@@ -1,14 +1,50 @@
 import styles from "./comments.module.scss";
-import React from "react";
+import React, { useState } from "react";
+import uuid from "react-uuid";
+import comments from "../../comments.json";
 
 export default function Comments() {
+  const [comment, setComment] = useState("");
+  const [commentList, setCommentList] = useState(comments);
+
+  function saveComment() {
+    setCommentList([...commentList, { id: uuid(), title: comment }]);
+    setComment("");
+  }
+
+  function deleteComment(id) {
+    let newComment = [...commentList].filter((item) => item.id !== id);
+    setCommentList(newComment);
+  }
+
   return (
     <div className={styles.container}>
-          <div className={styles.comments}></div>
-          <textarea rows='5' cols='20' placeholder="Напишите что-нибудь..."></textarea>
-          <div>
-              <button className={styles.button}>Отправить</button>
-              </div>
+      <h2>Комментарии</h2>
+      <div className={styles.comments}>
+        {commentList.map((item) => (
+          <div key={item.id}>
+            <div>{item.title}</div>
+            <button
+              className={styles.button}
+              onClick={() => deleteComment(item.id)}
+            >
+              Удалить
+            </button>
+          </div>
+        ))}
+      </div>
+      <textarea
+        rows="5"
+        cols="20"
+        placeholder="Напишите что-нибудь..."
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      ></textarea>
+      <div>
+        <button className={styles.button} onClick={saveComment}>
+          Отправить
+        </button>
+      </div>
     </div>
   );
 }
